0650f2933bc1fef0ce15e9241d4d5a83dc325827
aditya.wasnik <wasnikaditya56@gmail.com>
2023-09-09 14:26:40 +0530

    Added outlined text field

--- app/src/main/java/com/aditya/wasnik/firstprojectcompose/LoginActivity.kt
+++ app/src/main/java/com/aditya/wasnik/firstprojectcompose/LoginActivity.kt
@@ -8,8 +8,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -45,6 +53,7 @@ class LoginActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun LoginScreen() {
         val username = remember { mutableStateOf("") }
@@ -59,6 +68,56 @@ class LoginActivity : ComponentActivity() {
             Text(text = "Hello Aditya!", fontSize = 25.sp, color = Color.Blue,
                 fontFamily = FontFamily.Monospace, fontWeight = FontWeight.Bold
             )
+            Text(
+                text = "Welcome", fontSize = 25.sp, color = Color.Blue,
+                fontFamily = FontFamily.Monospace, fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "Back", fontSize = 25.sp, color = Color.Blue,
+                fontFamily = FontFamily.Monospace, fontWeight = FontWeight.Bold
+            )
+            OutlinedTextField(
+                value = username.value, 
+                onValueChange = {
+                    username.value = it
+                },
+                leadingIcon = {
+                    Icon(Icons.Default.Person, contentDescription = "person")
+                },
+                label = {
+                    Text(text = "Username")
+                },
+                placeholder = {
+                    Text(text = "enter username")
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+            
+            OutlinedTextField(
+                value = password.value,
+                onValueChange = {
+                    password.value = it
+                },
+                leadingIcon = {
+                    Icon(Icons.Default.Info, contentDescription = "person")
+                },
+                label = {
+                    Text(text = "Password")
+                },
+                placeholder = {
+                    Text(text = "enter password")
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+            
+            OutlinedButton(
+                onClick = { logged(username.value, password.value)},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp)
+            ) {
+                Text(text = "Login")
+            }
         }
 
     }
