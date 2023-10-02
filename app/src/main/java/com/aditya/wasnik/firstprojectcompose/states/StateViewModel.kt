df0f0020206ce81fa84f10eb03af6a6de47a106b
aditya.wasnik <wasnikaditya56@gmail.com>
2023-10-02 10:22:57 +0530

    Added state activity

---
+++ app/src/main/java/com/aditya/wasnik/firstprojectcompose/states/StateViewModel.kt
@@ -0,0 +1,14 @@
+package com.aditya.wasnik.firstprojectcompose.states
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class StateViewModel
+@Inject
+constructor() : ViewModel() {
+    val name: MutableState<String> = mutableStateOf("Aditya")
+}
